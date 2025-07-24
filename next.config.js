3845134813af2df168889aedb4c4030213b2d466
CorrosiveMoon <yaseen2003@yahoo.com>
2025-07-24 03:40:11 +0300

    fixed a typo, cleaned out comments

--- next.config.js
+++ next.config.js
@@ -1,20 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   trailingSlash: true,
-//   images: {
-//     unoptimized: true
-//   },
-//   // Only use basePath and assetPrefix for production (GitHub Pages)
-//   ...(process.env.NODE_ENV === 'production' && {
-//     basePath: '/portfolio',
-//     assetPrefix: '/portfolio/',
-//   }),
-//   distDir: 'out'
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
